d5d387d972b0cb777ed904d6c4f2a098f7f0c932
Silas Koemen <kpsilas@gmail.com>
2025-05-30 23:25:52 +0100

    Compare with xgboost as 'best' comparison, add random_state for reproducibility propagated through to node and sampling

--- examples/regression.py
+++ examples/regression.py
@@ -5,9 +5,11 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
 from sklearn.model_selection import train_test_split as TTS
+from xgboost import XGBRegressor
 
 from bdf.tree_classes.bdf_regressor import BDFRegressor
 
+"""
 ########################## Abalone dataset ######################
 data = pd.read_csv("../data/abaloneage.csv")
 X = data.iloc[:, 1:-1]
@@ -19,14 +21,14 @@ X_train, X_test, y_train, y_test = TTS(X, y, test_size=0.2, random_state=42)
 start_time = time()
 bdf = BDFRegressor(
     dist="sn",
-    prior_params={"mean": 0, "std": 3, "mean_alpha": 0, "m_alpha": 10},
-    n_trees=50,
-    reg_beta=0.5,
+    prior_params={"mean": 0, "std": 1, "mean_alpha": 0, "m_alpha": 5},
+    n_trees=25,
+    reg_beta=0.05,
     reg_lambda=0.01,
     max_depth=10,
     subsample=0.9,
     colsample=0.9,
-    min_samples_leaf=10,
+    min_samples_leaf=9,
 )
 bdf.fit(X_train.values, y_train.values, standardize_y=True)
 print(f"BDF fit time: {time() - start_time:.2f} seconds")
@@ -35,15 +37,15 @@ rf = RandomForestRegressor(max_depth=10, n_estimators=100, random_state=42)
 rf.fit(X_train.values, y_train.values)
 print(f"RF fit time: {time() - start_time:.2f} seconds")
 start_time = time()
-gb = GradientBoostingRegressor(max_depth=10, max_features=1.0, n_estimators=100, random_state=42)
-gb.fit(X_train.values, y_train.values)
-print(f"GB fit time: {time() - start_time:.2f} seconds")
+xgb = XGBRegressor(max_depth=10, n_estimators=100, random_state=42)
+xgb.fit(X_train.values, y_train.values)
+print(f"XGB fit time: {time() - start_time:.2f} seconds")
 # %%
 print(
-    f"MSE BDF: {np.mean((bdf.predict(X_test.values, method='mean', values={'total_size': 1000}) - y_test.values) ** 2)} | MSE RF: {np.mean((rf.predict(X_test.values) - y_test.values) ** 2)}, MSE GB: {np.mean((gb.predict(X_test.values) - y_test.values) ** 2)}"
+    f"MSE BDF: {np.mean((bdf.predict(X_test.values, method='mean', values={'total_size': 1000}) - y_test.values) ** 2)} | ",
+    f"MSE RF: {np.mean((rf.predict(X_test.values) - y_test.values) ** 2)} | MSE XGB: {np.mean((xgb.predict(X_test.values) - y_test.values) ** 2)} | "
 )
 # %%
-assert False
 ###################### Real estate dataset ######################
 data = pd.read_csv("../data/realestate.csv")
 data.describe()
@@ -54,9 +56,9 @@ X_train, X_test, y_train, y_test = TTS(X, y, test_size=0.2, random_state=1234)
 start_time = time()
 bdf = BDFRegressor(
     dist="sn",
-    prior_params={"mean": 0, "std": 3, "mean_alpha": 0, "m_alpha": 50},
-    n_trees=15,
-    reg_beta=0,
+    prior_params={"mean": 0, "std": 3, "mean_alpha": 0, "m_alpha": 5},
+    n_trees=25,
+    reg_beta=0.5,
     reg_lambda=0,
     max_depth=10,
     subsample=0.9,
@@ -70,13 +72,14 @@ rf = RandomForestRegressor(max_depth=10, n_estimators=100, random_state=42)
 rf.fit(X_train.values, y_train.values)
 print(f"RF fit time: {time() - start_time:.2f} seconds")
 start_time = time()
-gb = GradientBoostingRegressor(max_depth=10, max_features=1.0, n_estimators=100, random_state=42)
-gb.fit(X_train.values, y_train.values)
-print(f"GB fit time: {time() - start_time:.2f} seconds")
+xgb = XGBRegressor(max_depth=10, n_estimators=100, random_state=42)
+xgb.fit(X_train.values, y_train.values)
+print(f"XGB fit time: {time() - start_time:.2f} seconds")
 
 # %%
 print(
-    f"MSE BDF: {np.mean((bdf.predict(X_test.values, method='mean', values={'total_size': 1000}) - y_test.values) ** 2)} | MSE RF: {np.mean((rf.predict(X_test.values) - y_test.values) ** 2)}, MSE GB: {np.mean((gb.predict(X_test.values) - y_test.values) ** 2)}"
+    f"MSE BDF: {np.mean((bdf.predict(X_test.values, method='mean', values={'total_size': 1000}) - y_test.values) ** 2)} | ",
+    f"MSE RF: {np.mean((rf.predict(X_test.values) - y_test.values) ** 2)} | MSE XGB: {np.mean((xgb.predict(X_test.values) - y_test.values) ** 2)} | "
 )
 # %%
 ######################## Wine quality dataset ######################
@@ -89,12 +92,12 @@ X_train, X_test, y_train, y_test = TTS(X, y, test_size=0.2, random_state=1234)
 start_time = time()
 bdf = BDFRegressor(
     dist="sn",
-    prior_params={"mean": 0, "std": 3, "mean_alpha": 0, "m_alpha": 50},
-    n_trees=15,
-    reg_beta=0,
+    prior_params={"mean": 0, "std": 10, "mean_alpha": 0, "m_alpha": 5},
+    n_trees=25,
+    reg_beta=0.05,
     reg_lambda=0,
     max_depth=10,
-    subsample=0.9,
+    subsample=0.8,
     colsample=1.0,
     min_samples_leaf=10,
 )
@@ -105,13 +108,14 @@ rf = RandomForestRegressor(max_depth=10, n_estimators=100, random_state=42)
 rf.fit(X_train.values, y_train.values)
 print(f"RF fit time: {time() - start_time:.2f} seconds")
 start_time = time()
-gb = GradientBoostingRegressor(max_depth=10, max_features=1.0, n_estimators=100, random_state=42)
-gb.fit(X_train.values, y_train.values)
-print(f"GB fit time: {time() - start_time:.2f} seconds")
+xgb = XGBRegressor(max_depth=10, n_estimators=100, random_state=42)
+xgb.fit(X_train.values, y_train.values)
+print(f"XGB fit time: {time() - start_time:.2f} seconds")
 # %%
 print(
-    f"MSE BDF: {np.mean((bdf.predict(X_test.values, method='mean', values={'total_size': 1000}) - y_test.values) ** 2)} | MSE RF: {np.mean((rf.predict(X_test.values) - y_test.values) ** 2)}, MSE GB: {np.mean((gb.predict(X_test.values) - y_test.values) ** 2)}"
-)
+    f"MSE BDF: {np.mean((bdf.predict(X_test.values, method='mean', values={'total_size': 1000}) - y_test.values) ** 2)} | ",
+    f"MSE RF: {np.mean((rf.predict(X_test.values) - y_test.values) ** 2)} | MSE XGB: {np.mean((xgb.predict(X_test.values) - y_test.values) ** 2)} | "
+)"""
 # %%
 ########################## Parkinsons dataset ######################
 data = pd.read_csv("../data/parkinsons_updrs.csv")
@@ -144,14 +148,14 @@ X_train, X_test, y_train, y_test = TTS(X, y, test_size=0.2, random_state=1234)
 start_time = time()
 bdf = BDFRegressor(
     dist="sn",
-    prior_params={"mean": 0, "std": 3, "mean_alpha": 0, "m_alpha": 50},
-    n_trees=15,
-    reg_beta=1,
-    reg_lambda=1,
+    prior_params={"mean": 0, "std": 3, "mean_alpha": 0, "m_alpha": 10},
+    n_trees=25,
+    reg_beta=0.1,
+    reg_lambda=0.01,
     max_depth=15,
     subsample=0.9,
     colsample=0.9,
-    min_samples_leaf=20,
+    min_samples_leaf=10,
 )
 bdf.fit(X_train.values, y_train.values, standardize_y=True)
 print(f"BDF fit time: {time() - start_time:.2f} seconds")
@@ -160,13 +164,14 @@ rf = RandomForestRegressor(max_depth=10, n_estimators=100, random_state=42)
 rf.fit(X_train.values, y_train.values)
 print(f"RF fit time: {time() - start_time:.2f} seconds")
 start_time = time()
-gb = GradientBoostingRegressor(max_depth=10, max_features=1.0, n_estimators=100, random_state=42)
-gb.fit(X_train.values, y_train.values)
-print(f"GB fit time: {time() - start_time:.2f} seconds")
+xgb = XGBRegressor(max_depth=10, n_estimators=100, random_state=42)
+xgb.fit(X_train.values, y_train.values)
+print(f"XGB fit time: {time() - start_time:.2f} seconds")
 # %%
 print(
-    f"MSE BDF: {np.mean((bdf.predict(X_test.values, method='median') - y_test.values) ** 2)} | MSE RF: {np.mean((rf.predict(X_test.values) - y_test.values) ** 2)}, MSE GB: {np.mean((gb.predict(X_test.values) - y_test.values) ** 2)}"
+    f"MSE BDF: {np.mean((bdf.predict(X_test.values, method='median', values={'total_size': 1000}) - y_test.values) ** 2):.3f} | ",
+    f"MSE RF: {np.mean((rf.predict(X_test.values) - y_test.values) ** 2):.3f} | MSE XGB: {np.mean((xgb.predict(X_test.values) - y_test.values) ** 2):.3f} | ",
 )
 # %%
-bdf.plot_tree(0)
+# bdf.plot_tree(0)
 # %%
